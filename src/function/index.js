c58e38725af6b0b5b5be39b865e73182dbe1148f
rose <rosewang0303@gmail.com>
2021-11-10 11:20:08 +0800

    城市名稱處理

--- src/function/index.js
+++ src/function/index.js
@@ -8,3 +8,11 @@ export function formatDate(time) {
     let format = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate()
     return format;
 }
+/** 
+ * api回傳城市名稱format
+ * @param   {Object}    item    單筆回傳資料
+ */
+ export function formatCity(item) {
+    let format = item.City?item.City:item.Address.substr(0, 3);
+    return format;
+}
