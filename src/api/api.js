7d3a6999dc514d56db3b3f97bbe090a359528152
rose <rosewang0303@gmail.com>
2021-11-14 00:18:00 +0800

    api錯誤訊息顯示

--- src/api/api.js
+++ src/api/api.js
@@ -68,20 +68,30 @@ webTokenRequestV2.interceptors.response.use(
       switch (error.response.status) {
         case 400:
           // 發生錯誤
+          alert("400 發生錯誤")
+          console.error("400 發生錯誤");
           break
         case 404:
           // 你要找的頁面不存在
+          alert("404 你要找的頁面不存在")
+          console.error("404 你要找的頁面不存在");
           break
         case 500:
           // 程式發生錯誤，請聯絡管理員
+          alert("500 程式發生錯誤，請聯絡管理員")
+          console.error("500 程式發生錯誤，請聯絡管理員");
           break
         default:
           // 系統發生非預期錯誤，請聯絡管理員
+          alert("系統發生非預期錯誤，請聯絡管理員")
+          console.error("系統發生非預期錯誤，請聯絡管理員");
       }
       console.log(error.response.status);
     } 
     if (!window.navigator.onLine) {
       // 沒有網路連線
+      alert("沒有網路連線")
+      console.error("沒有網路連線");
       return;
     }
     return Promise.reject(error.response);
@@ -98,20 +108,30 @@ webTokenRequestV3.interceptors.response.use(
       switch (error.response.status) {
         case 400:
           // 發生錯誤
+          alert("400 發生錯誤")
+          console.error("400 發生錯誤");
           break
         case 404:
           // 你要找的頁面不存在
+          alert("404 你要找的頁面不存在")
+          console.error("404 你要找的頁面不存在");
           break
         case 500:
           // 程式發生錯誤，請聯絡管理員
+          alert("500 程式發生錯誤，請聯絡管理員")
+          console.error("500 程式發生錯誤，請聯絡管理員");
           break
         default:
           // 系統發生非預期錯誤，請聯絡管理員
+          alert("系統發生非預期錯誤，請聯絡管理員")
+          console.error("系統發生非預期錯誤，請聯絡管理員");
       }
       console.log(error.response.status);
     } 
     if (!window.navigator.onLine) {
       // 沒有網路連線
+      alert("沒有網路連線")
+      console.error("沒有網路連線");
       return;
     }
     return Promise.reject(error.response);
