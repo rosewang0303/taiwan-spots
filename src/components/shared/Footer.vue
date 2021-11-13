6eaca34eb510f7aa6b98f46cebda12017267531c
rose <rosewang0303@gmail.com>
2021-11-13 19:17:16 +0800

    footer加上我的連結

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <div>#The F2E 3rd Week01 #Rose</div>
+        <div>#The F2E 3rd Week01 <a href="https://2021.thef2e.com/users/6296432819610583251" target="_blank">#Rose</a></div>
     </footer>
 </template>
 <script>
@@ -17,5 +17,12 @@ footer {
     color: $white;
     font-size: 14px;
     line-height: 19px;
+    a {
+        font-weight: bold;
+        color: $white;
+        &:visited, &:hover {
+            color: $white;
+        }
+    }
 }
 </style>
